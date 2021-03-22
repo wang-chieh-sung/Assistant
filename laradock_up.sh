e71dac7056a15bb6cc8b94ac0fdbd5c7da53fc8b
Jason <71313142+wang-chieh-sung@users.noreply.github.com>
2021-03-22 13:08:51 +0800

    Update laradock_up.sh
    restore vender

--- laradock_up.sh
+++ laradock_up.sh
@@ -3,6 +3,7 @@ cp -a laradock_env/. laradock
 pushd laradock
 docker-compose up -d apache2 mysql phpmyadmin workspace
 docker-compose exec -u laradock workspace composer create-project --prefer-dist laravel/laravel .
+docker-compose exec -u laradock workspace composer install
 docker-compose exec -u laradock workspace php artisan migrate
 docker-compose exec -u laradock workspace composer require laravel/breeze --dev
 docker-compose exec -u laradock workspace php artisan breeze:install
